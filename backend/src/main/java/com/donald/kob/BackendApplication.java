d0601fd065cb02ce0c894d16221c8e141c1d93ad
junhui_deng <1376825153@qq.com>
2023-09-02 02:00:27 +0800

    backend init

---
+++ backend/src/main/java/com/donald/kob/BackendApplication.java
@@ -0,0 +1,20 @@
+package com.donald.kob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@Controller
+public class BackendApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BackendApplication.class, args);
+    }
+    @RequestMapping("/index")
+    public static String index(){
+        return "index.html";
+    }
+}
---
+++ backend/src/main/java/com/donald/kob/controller/Index.java
@@ -0,0 +1,15 @@
+package com.donald.kob.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/index")
+public class Index {
+    @RequestMapping("/info")
+    public String getInfo(){
+        return "index info";
+    }
+}
