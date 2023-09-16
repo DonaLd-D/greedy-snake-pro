fc2040800fbda88d5f0bda2d5ec3443e0259447f
junhui_deng <1376825153@qq.com>
2023-09-16 17:55:48 +0800

    feat:getinfo api

--- backend/src/main/java/com/donald/kob/controller/user/AccountController.java
+++ backend/src/main/java/com/donald/kob/controller/user/AccountController.java
@@ -1,23 +1,27 @@
-package com.donald.kob.controller.user.account;
+package com.donald.kob.controller.user;
 
+import com.donald.kob.service.user.account.InfoService;
 import com.donald.kob.service.user.account.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
-public class LoginController {
+public class AccountController {
     @Autowired
     LoginService loginService;
+    @Autowired
+    InfoService infoService;
 
     @PostMapping("/user/account/token")
     public Map<String,String> getToken(@RequestBody Map<String,String> req){
         String username=req.get("username");
         String password=req.get("password");
         return loginService.getToken(username,password);
+    }
+    @GetMapping("/user/account/info")
+    public Map<String,String> getInfo(){
+        return infoService.getinfo();
     }
 }
--- backend/src/main/java/com/donald/kob/service/impl/user/account/InfoServiceImpl.java
+++ backend/src/main/java/com/donald/kob/service/impl/user/account/InfoServiceImpl.java
@@ -1,15 +1,30 @@
 package com.donald.kob.service.impl.user.account;
 
+import com.donald.kob.pojo.User;
+import com.donald.kob.service.impl.utils.UserDetailsImpl;
 import com.donald.kob.service.user.account.InfoService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Service
 public class InfoServiceImpl implements InfoService {
     @Override
     public Map<String, String> getinfo() {
+        UsernamePasswordAuthenticationToken authentication=
+                (UsernamePasswordAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
 
-        return null;
+        UserDetailsImpl loginUser=(UserDetailsImpl) authentication.getPrincipal();
+        User user=loginUser.getUser();
+
+        Map<String,String> ans=new HashMap<>();
+        ans.put("msg","success");
+        ans.put("id",user.getId().toString());
+        ans.put("username",user.getUsername());
+        ans.put("avatar",user.getAvatar());
+        return ans;
     }
 }
