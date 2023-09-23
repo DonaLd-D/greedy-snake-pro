6289440754068df6fa5519ca4d87b0826be9ce43
junhui_deng <1376825153@qq.com>
2023-09-23 22:06:28 +0800

    feat:bot data

---
+++ backend/src/main/java/com/donald/kob/mapper/BotMapper.java
@@ -0,0 +1,9 @@
+package com.donald.kob.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.donald.kob.pojo.Bot;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface BotMapper extends BaseMapper<Bot> {
+}
